b08ce13163851739b7d3fd5d44abca1e7466f963
xm <yangyixin@stu.pku.edu.cn>
2025-10-27 01:49:44 +0800

    AST

--- note.md
+++ note.md
@@ -3,4 +3,6 @@
 ```bash
 # 启动 docker 容器，挂载项目目录到容器内
 docker run -it --rm -v "D:/wksp/compilers/SysYrc":/root/compiler maxxing/compiler-dev bash
+
+cargo run -- mode hello.c -o hello.s
 ```
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,23 @@ use std::env::args;
 use std::fs::read_to_string;
 use std::io::Result;
 
-// 引用 lalrpop 生成的解析器
-// 因为我们刚刚创建了 sysy.lalrpop, 所以模块名是 sysy
+pub mod ast;
+
 lalrpop_mod!(sysy);
 
 fn main() -> Result<()> {
-  // 解析命令行参数
-  let mut args = args();
-  args.next();
-  let mode = args.next().unwrap();
-  let input = args.next().unwrap();
-  args.next();
-  let output = args.next().unwrap();
+    // sysyrc <mode> <input> -o <output>
+    let mut args = args();
+    args.next();
+    let mode = args.next().unwrap();
+    let input = args.next().unwrap();
+    args.next();
+    let output = args.next().unwrap();
 
-  // 读取输入文件
-  let input = read_to_string(input)?;
+    let input = read_to_string(input)?;
 
-  // 调用 lalrpop 生成的 parser 解析输入文件
-  let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+    let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
 
-  // 输出解析得到的 AST
-  println!("{}", ast);
-  Ok(())
+    println!("{:#?}", ast);
+    Ok(())
 }
